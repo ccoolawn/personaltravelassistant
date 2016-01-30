67bbc5a0d789c8a1ac13aec29055873b5f75ddb0
Eric Zuniga <ericzuniga24@gmail.com>
2016-01-30 10:50:20 -0800

    corrected 'fetch'

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,5 @@
 var User = mongoose.model('User');
-var travel = require('request'); 
+//var travel = require('request'); 
 var http = require('http');
 
 module.exports = (function() {
@@ -76,44 +76,44 @@ module.exports = (function() {
 				}
 			})
 		}, 
-		// fetch: function(request, response){
-		// 	console.log("Server / Ctrl/ Users - Fetch 4");
-		// 	var place = {};
-		// 	var that = this;
-		// 		that.result1 = request.body.city;
-		// 		that.options1 = null;
-		// 		that.details1 = '';
-		// 		that.weather = '';
-		// 		that.recipies = '';
-		// 		// console.log(that.options1);
-		// 		// console.log(that.details1);
-		// 	var pendingTask;
-		// 		if (that.result1 === undefined) {
-		// 			that.result1 = "";
-		// 		}
-		// 		that.change = function() {
-		// 			if (pendingTask) {
-		// 				clearTimeout(pendingTask);
-		// 			}
-		// 			pendingTask = setTimeout(fetch, 800);
-		// 		};
-		// 		console.log(that.result1);
+		fetch: function(request, response){
+			console.log("Server / Ctrl/ Users - Fetch 4");
+			var place = {};
+			var that = this;
+				that.result1 = request.body.city;
+				that.options1 = null;
+				that.details1 = '';
+				that.weather = '';
+				that.recipies = '';
+				// console.log(that.options1);
+				// console.log(that.details1);
+			var pendingTask;
+				if (that.result1 === undefined) {
+					that.result1 = "";
+				}
+				that.change = function() {
+					if (pendingTask) {
+						clearTimeout(pendingTask);
+					}
+					pendingTask = setTimeout(fetch, 800);
+				};
+				console.log(that.result1);
 
 
 
-		// 		function travelGet (data, callback){
-		// 			travel.get('http://api.openweathermap.org/data/2.5/weather?q=' + data + '&units=imperial&APPID=9491e0f2bd9ec591e2f391ec993acaf8',function(error, res){
-		// 				if (error)
-		// 					console.log(error);
-		// 				else {;
-		// 					callback(res.body);
-		// 				}
-		// 			});
-		// 		}
+				function travelGet (data, callback){
+					travel.get('http://api.openweathermap.org/data/2.5/weather?q=' + data + '&units=imperial&APPID=9491e0f2bd9ec591e2f391ec993acaf8',function(error, res){
+						if (error)
+							console.log(error);
+						else {;
+							callback(res.body);
+						}
+					});
+				}
 
-		// 	travelGet(that.result1, function(q){
-		// 		response.json(q);
-		// 	});
-		// }
+			travelGet(that.result1, function(q){
+				response.json(q);
+			});
+		}
 	}
 })
