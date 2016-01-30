e1b246ef11fb3fe609cdf5ef882892f334125555
ccoolawn <cornell1001@mac.com>
2016-01-29 21:21:13 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 
@@ -19,36 +18,4 @@ require("./server/config/mongoose.js");
 
 var routes = require("./server/config/routes.js");
 routes(app);
-=======
-var express = require('express')
-var app = express()
-app.listen(1337);
 
-// Sockets
-// io = require('socket.io').listen(server)
-
-// Body Parser
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
-// Session
-var session = require('express-session')
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
-
-
-app.use(express.static(__dirname + '/client'))
-
-// Mongoose
-require('./server/config/mongoose.js');
-// HTTP Routes`	
-require('./server/config/routes.js')(app);
-// Socket Routes
-// require('./server/config/socket.routes.js')(app);
-
-
-
->>>>>>> origin/master
